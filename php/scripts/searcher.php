a09676f4f32e0e6956c2cc3879561238baf8353f
LauraMT94 <laura-maldonado@hotmail.es>
2016-05-26 19:51:02 +0200

    .

--- php/scripts/searcher.php
+++ php/scripts/searcher.php
@@ -7,17 +7,10 @@
     
     if ($result->num_rows>0){
         while($row = mysqli_fetch_array($result)){
-<<<<<<< HEAD
-            echo '<div class="element_sub_toolbar" onclick="alert(\'HOLA\');window.location=\'incidencia.html?id='.$row['ID'].'\';"> 
-                ID:'.$row['ID'].'<br>
-                $title:'.$row['TITULO'].'<br>
-                $date:'.$row['FECHA'].'
-=======
             echo '<div class="element_sub_toolbar" onclick="window.location=\'incidencia.html?id='.$row['ID'].'\';"> 
                 ID:'.$row['ID'].'<br><br>
                 <span style="font-weight: bold; color: black;">'.$row['TITULO'].'</span><br><br>
                 '.$row['FECHA'].'
->>>>>>> 02971a2c8e448f91024f0e67834622b6e9edc359
             </div>';
         }    
     }else{
